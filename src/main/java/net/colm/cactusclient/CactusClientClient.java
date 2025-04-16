b8b2020526b0f06478e480f9f4534e1ca7878eca
Cactus <colm.mcloughlinirl@gmail.com>
2025-04-16 14:35:46 +0100

    a

---
+++ src/main/java/net/colm/cactusclient/CactusClientClient.java
@@ -0,0 +1,9 @@
+package net.colm.cactusclient;
+import net.fabricmc.api.ClientModInitializer;
+
+public class CactusClientClient implements net.fabricmc.api.ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
